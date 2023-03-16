b58793c392e7db635fd6e3d9e984a1ab9437c7c3
Emeric Martineau <11473190+emeric-martineau@users.noreply.github.com>
2023-03-16 18:05:51 +0100

    Add ng-bootstrap

--- 3d-printer-for-hobbies-app/src/app/app.module.ts
+++ 3d-printer-for-hobbies-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -18,9 +19,13 @@ import { PrintTableComponent } from './components/print-table/print-table.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NgbModule
+  ]
 })
 export class AppModule { }
